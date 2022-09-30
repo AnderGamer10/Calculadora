12c26a165b97b8937cdaf2e81b51e25d5ad0eed6
2dam3 <2dam3@HZ114485.H012108.NET>
2022-09-30 13:03:50 +0200

    first commit

---
+++ src/application/Controller.java
@@ -0,0 +1,62 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Controller  {
+	@FXML
+    private Button suma;
+
+    @FXML
+    private Button multiplicacion;
+
+    @FXML
+    private Button C;
+
+    @FXML
+    private Button siete;
+
+    @FXML
+    private Button restar;
+
+    @FXML
+    private Button ocho;
+
+    @FXML
+    private Button dos;
+
+    @FXML
+    private Button seis;
+
+    @FXML
+    private Button nueve;
+
+    @FXML
+    private Button cuatro;
+
+    @FXML
+    private Button coma;
+
+    @FXML
+    private Button division;
+
+    @FXML
+    private Button cinco;
+
+    @FXML
+    private Button cero;
+
+    @FXML
+    private Button borrar;
+
+    @FXML
+    private Button uno;
+
+    @FXML
+    private Button igual;
+
+    @FXML
+    private Button tres;
+}
