c73d29a4495e413631415685db9bf5de26d30125
2dam3 <2dam3@HZ114485.H012108.NET>
2022-10-04 13:12:45 +0200

    haciendoControlador

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,62 +1,96 @@
 package application;
 
-import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 public class Controller  {
-	@FXML
-    private Button suma;
+	 @FXML
+	    private Button suma;
+
+	    @FXML
+	    private Button multiplicacion;
+
+	    @FXML
+	    private Button C;
+
+	    @FXML
+	    private Button siete;
+
+	    @FXML
+	    private Button restar;
+
+	    @FXML
+	    private Button ocho;
+
+	    @FXML
+	    private Button dos;
+
+	    @FXML
+	    private Button seis;
+
+	    @FXML
+	    private Button nueve;
+
+	    @FXML
+	    private Button cuatro;
+
+	    @FXML
+	    private Button coma;
+
+	    @FXML
+	    private TextField texto;
+
+	    @FXML
+	    private Button division;
+
+	    @FXML
+	    private Button cinco;
+
+	    @FXML
+	    private Button cero;
+
+	    @FXML
+	    private Button borrar;
+
+	    @FXML
+	    private Button uno;
+
+	    @FXML
+	    private Button igual;
+
+	    @FXML
+	    private Button tres;
+
+	    private Boolean haciendoOperacion = true;
+
 
     @FXML
-    private Button multiplicacion;
+    void anyadirValor(ActionEvent event) {
+    	texto.setText(texto.getText() + ((Button)event.getSource()).getText());
+    	haciendoOperacion = true;
+    }
 
     @FXML
-    private Button C;
+    void anyadirOperador(ActionEvent event) {
+    	if(haciendoOperacion){
+    		texto.setText(texto.getText() + ((Button)event.getSource()).getText());
+    		haciendoOperacion = false;
+    	}
+    }
 
     @FXML
-    private Button siete;
+    void vaciarTexto(ActionEvent event) {
+    	texto.setText("");
+    	haciendoOperacion = true;
+    }
 
-    @FXML
-    private Button restar;
+    public void eliminarValor(){
+		if(texto.getText().length() != 0){
+			texto.setText(texto.getText().substring(0, texto.getText().length()-1));
+		}
+	}
 
-    @FXML
-    private Button ocho;
 
-    @FXML
-    private Button dos;
-
-    @FXML
-    private Button seis;
-
-    @FXML
-    private Button nueve;
-
-    @FXML
-    private Button cuatro;
-
-    @FXML
-    private Button coma;
-
-    @FXML
-    private Button division;
-
-    @FXML
-    private Button cinco;
-
-    @FXML
-    private Button cero;
-
-    @FXML
-    private Button borrar;
-
-    @FXML
-    private Button uno;
-
-    @FXML
-    private Button igual;
-
-    @FXML
-    private Button tres;
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,16 +12,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+	        primaryStage.setTitle("Calculadora");
+	        primaryStage.setScene(new Scene(root, 360, 380));
+	        primaryStage.setResizable(false);
+	        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
